771cc35775026b06827e33cfa4d023d202bc3d2a
Thibault Leruitte <thibault@leruitte.name>
2014-11-19 16:16:46 +0100

    Optimization 5: pre-load templates

--- app/components/author/author.js
+++ app/components/author/author.js
@@ -2,6 +2,17 @@
 
 angular.module('app.author', [])
 
+.run(function ($templateCache) {
+	$templateCache.put('components/author/author.html',
+		'<a href="http://twitter.com/{{ user.screen_name }}">' +
+			'{{ user.name }}' +
+			'<span class="desc">' +
+				'({{ user.screen_name }})' +
+			'</span>' +
+		'</a>'
+	);
+})
+
 .directive('appAuthor', function () {
 	return {
 		restrict: 'A',
